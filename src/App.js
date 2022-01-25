7e47a130683993b6b337ad761372b8daa185c0cb
oryxwhite <93627129+oryxwhite@users.noreply.github.com>
2022-01-25 08:07:29 -0500

    added sorting options

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Country from './Country'
 import { useState } from 'react'
 import { useEffect } from 'react'
 
-
 function App() {
 
   const [countries, setCountries] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [sortType, setSortType] = useState('')
 
   useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
@@ -20,70 +20,101 @@ function App() {
         return 0;
       }))
       .then(data => setCountries(data))
+      console.log('fetch render')
   }, [])
 
-  // function sort(event) {
-  //   if (event.target.value == "AZ") {
-  //     setCountries(prevCountries => {
-  //       return prevCountries.sort((a, b) => {
-  //         if (a.name.common.toLowerCase() < b.name.common.toLowerCase())
-  //           return -1;
-  //       if (a.name.common.toLowerCase() > b.name.common.toLowerCase())
-  //           return 1;
-  //       return 0;
-  //       })
-  //   })}
+  useEffect(() => {
+    function sortCountries(sortType) {
+      if (sortType == "AZ") {
+        setCountries(prevCountries => {
+          const sortedCountries = prevCountries.sort((a, b) => {
+            if (a.name.common.toLowerCase() < b.name.common.toLowerCase())
+              return -1;
+          if (a.name.common.toLowerCase() > b.name.common.toLowerCase())
+              return 1;
+          return 0;
+          })
+          return sortedCountries;
+      })}
 
-  //   if (event.target.value == 'ZA') {
-  //     setCountries(prevCountries => {
-  //       return prevCountries.sort((a,b) => {
-  //         return a.population - b.population;
-  //       })
-  //     })
-  //   }
-    
-  // }
-  // console.log(countries[0].name.common)
+      if (sortType == "ZA") {
+        setCountries(prevCountries => {
+          const sortedCountries = prevCountries.sort((a, b) => {
+            if (a.name.common.toLowerCase() > b.name.common.toLowerCase())
+              return -1;
+          if (a.name.common.toLowerCase() < b.name.common.toLowerCase())
+              return 1;
+          return 0;
+          })
+          return sortedCountries;
+      })}
+      
+      if (sortType == 'popA') {        
+        setCountries(prevCountries => {
+          return prevCountries.sort((a,b) => {
+            return a.population - b.population;
+          })
+        })
+      }
+
+      if (sortType == 'popD') {        
+        setCountries(prevCountries => {
+          return prevCountries.sort((a,b) => {
+            return b.population - a.population;
+          })
+        })
+      }
+  }
+  sortCountries(sortType)
+  setSortType('')
+  }, [sortType])
 
   
+  {/* the filter function returns an array of the country objects that match the name of the country from the search bar
+      the map function returns an array of <Country /> components with the given props from the country objects
+  */}
 
+  function mapCountries() {
+    console.log('map countries')
+
+    return countries
+    .filter(val => {
+      if (searchTerm == '') {
+        return val
+      } else if (val.name.common.toLowerCase().includes(searchTerm.toLowerCase())) {
+        return val
+      }
+    })
+    .map((country, key) => {
+      return <Country 
+        key={key}
+        name={country.name.common}
+        population={country.population}
+        flag={country.flags.png}
+        capital={country.capital}
+        languages={country.languages}
+        />
+    })
+  } 
+
+  
 
   return (
     <div className="main">
       <div className="header">
         <input type="text" placeholder="Search.." onChange={event => setSearchTerm(event.target.value)} />
         
-          <select name="membership" id="membership" onClick={console.log(event => console.log(event))}>
-            <option value="AZ">Country A - Z</option>
-            <option value="ZA">Country Z - A</option>
-            <option value="silver">Population Descending</option>
-            <option value="Gold">Population Ascending</option>
-          </select>
-
+        <div><select name="membership" id="membership" onChange={event => setSortType(event.target.value)}>
+          <option value="AZ">Country A - Z</option>
+          <option value="ZA">Country Z - A</option>
+          <option value="popD">Population Descending</option>
+          <option value="popA">Population Ascending</option>
+        </select>
+        </div>
       </div>
+
       <div className="countries">
-        {/* the filter function returns an array of the country objects that match the name of the country from the search bar
-            the map function returns an array of <Country /> components with the given props from the country objects
-        */}
-        
-        {countries
-        .filter(val => {
-          if (searchTerm == '') {
-            return val
-          } else if (val.name.common.toLowerCase().includes(searchTerm.toLowerCase())) {
-            return val
-          }
-        })
-        .map((country, key) => {
-          return <Country 
-            key={key}
-            name={country.name.common}
-            population={country.population}
-            flag={country.flags.png}
-            capital={country.capital}
-            
-            />
-        })}
+        {mapCountries()}
       </div>
     </div>
   )
--- src/Country.js
+++ src/Country.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 export default function country(props) {
+  
   return (
     <div className='country'>
         <img className='flag' src={props.flag}></img>
         <div className='info'>
           <div className='name'>{props.name}</div> 
-          <div>Population: {props.population}</div>
+          <div>Population: {props.population.toLocaleString("en-US")}</div>
           <div>Capital: {props.capital}</div>
-          {/* <div>Languages:  </div> */}
+          <div>Languages: {}</div>
         </div>
     </div>
   )  
